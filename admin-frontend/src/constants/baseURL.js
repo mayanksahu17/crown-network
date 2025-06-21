c704b983c44c7a6bd5c7394ebba0966800c990a4
mayanksahu17 <mayank0real0world@gmail.com>
2025-06-21 14:34:17 +0530

    Add baseURL configuration and README with connection details

--- admin-frontend/src/constants/baseURL.js
+++ admin-frontend/src/constants/baseURL.js
@@ -1,4 +1,6 @@
 // export default process.env.API_URL + "/api";
-export default "http://localhost:5000/api";
+// export default "http://localhost:5000/api";
 // export default "https://admin.crownbankers.com/api";
 // export default "http://66.29.136.199:81/api";
+
+export  default  "http://162.0.234.7:5000/api"
---
+++ readme.md
@@ -0,0 +1,10 @@
+IP address 162.0.234.7
+Port 22
+Username root
+Password Y2oy61uHO0jl1bqsguE3
+
+
+
+make changes live 
+
+https://chatgpt.com/c/68500e4d-9ec0-800a-ac14-5fef7e553eab
